ee9bd3a36ed14d0bd64f1f3e0877324e09a4b699
lisx9 <1723263422@qq.com>
2024-02-27 14:48:06 +0800

    增加数据集

--- data/dataReduction.py
+++ data/dataReduction.py
@@ -1,5 +1,5 @@
 from dataLoader import MyDataset
 
-my_datasset = MyDataset("D:/lipreading/data", transform=False)
+my_datasset = MyDataset("D:/lipreading/graduation/dataset", transform=False)
 video, label = my_datasset.__getitem__(0)
 print(video, label)
