50033fac0eb5c9e9a39ff43e9f3102341e5a7ddf
lisx9 <1723263422@qq.com>
2024-02-28 14:53:46 +0800

    s3d

--- data/dataReduction.py
+++ data/dataReduction.py
@@ -4,6 +4,6 @@ my_datasset = MyDataset("D:/lipreading/graduation/dataset", transform=False, con
 video, label = my_datasset.__getitem__(1)
 print(video, label)
 print(video.shape)
-feature = my_datasset.get_swin_feature(video)
+feature = my_datasset.get_s3d_feature(video)
 print(feature)
 print(feature.shape)
