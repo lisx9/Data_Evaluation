94a05b0a04e94fea3c61e517b70cb2b4e4e15f4b
lisx9 <1723263422@qq.com>
2024-02-28 15:08:21 +0800

    更改特征维度

--- data/dataReduction.py
+++ data/dataReduction.py
@@ -2,6 +2,7 @@ from dataLoader import MyDataset
 
 my_datasset = MyDataset("D:/lipreading/graduation/dataset", transform=False, convert_gray=False)
 video, label = my_datasset.__getitem__(1)
+video = video.transpose(1, 3)
 print(video, label)
 print(video.shape)
 feature = my_datasset.get_s3d_feature(video)
